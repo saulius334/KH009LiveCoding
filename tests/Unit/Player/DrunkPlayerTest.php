d2b2853f34b4b7f528599307aa5e6b0a914f09e4
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-11-17 19:05:22 +0200

    seperate tests

--- tests/Unit/Player/DrunkPlayerTest.php
+++ tests/Unit/Player/DrunkPlayerTest.php
@@ -10,14 +10,22 @@ use PHPUnit\Framework\TestCase;
 
 class DrunkPlayerTest extends TestCase
 {
-    public function testDrunkPlayerRetrunsRandomLetter()
+    public function testDrunkPlayerReturnsLetter()
     {
         $state = $this->createMock(GameState::class);
         $drunkPlayer = new DrunkPlayer();
         $letter = $drunkPlayer->guessLetter($state);
         $this->assertIsString($letter);
         $this->assertSame(strlen($letter), 1);
+    }
+    public function testDrunkPlayerDosentReturnSameLetter()
+    {
+        $state = $this->createMock(GameState::class);
+        $drunkPlayer = new DrunkPlayer();
+
+        $letter = $drunkPlayer->guessLetter($state);
         $letter2 = $drunkPlayer->guessLetter($state);
+
         $this->assertNotEquals($letter, $letter2);
     }
 }
